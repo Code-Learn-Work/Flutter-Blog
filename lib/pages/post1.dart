9b466817d3042711047564037c2abb06cfdeb637
Mayank Khanna <khanna98@outlook.com>
2019-09-30 23:54:58 +0530

    First Blog Post

---
+++ lib/pages/post1.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.light,
+  ));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+  ));
+}
